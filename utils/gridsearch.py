f4b00191e916a3844ad1bbdb17ffc96ff1475937
yanghongshen <yang2819281@gmail.com>
2023-09-22 13:16:36 +0900

    grid search mvp

--- utils/gridsearch.py
+++ utils/gridsearch.py
@@ -7,15 +7,22 @@ param_grid = {
     "param3": random.sample(range(-50, 50), 6),
 }
 
-def est(param1, param2, param3):
-    val1 = math.sin(param1)
-    val2 = math.cos(param2)
-    val3 = math.sin(param3)
+datafeeds = random.sample(range(-50, 50), 5)
+
+def est(datafeeds, param_dict):
+    # {param1: 1, param2: 2, param3:3}
+    val1 = math.sin(param_dict.get('param1'))
+    val2 = math.cos(param_dict.get('param2'))
+    val3 = math.sin(param_dict.get('param3'))
     return val1 + val2 + val3
+
+def scoring(result):
+    score = result**2
+    return score
 
 # Try to follow the practice of sklearn
 # https://scikit-learn.org/stable/modules/generated/sklearn.model_selection.GridSearchCV.html
-def gridsearch(estimator, param_grid):
+def gridsearch(estimator, param_grid, scoring, datafeeds):
     best_score = None
     best_params = None
 
@@ -24,9 +31,15 @@ def gridsearch(estimator, param_grid):
 
     for params in param_combinations:
         param_dict = {key: val for key, val in zip(param_grid.keys(), params)}
-        score = estimator(**param_dict)
+        result = estimator(datafeeds, param_dict)
+        score = scoring(result)
         if best_score is None or score > best_score:
             best_score = score
             best_params = param_dict
     
     return best_score, best_params
+
+if __name__ == '__main__':
+    # res = gridsearch(est, param_grid, scoring, datafeeds)
+    # print(res)
+    print(param_grid)
