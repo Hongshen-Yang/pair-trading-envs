a327eff247f00fa29cbda12653215a46566e3602
Yang <hyan212@aucklanduni.ac.nz>
2023-12-28 21:05:39 +1300

    bring the work home

--- params.py
+++ params.py
@@ -16,6 +16,7 @@ symbols = ['BTCUSD', 'BTCGBP', 'BTCEUR', 'BTCRUB', 'ETHUSDT', 'ETHGBP', 'ETHEUR'
 
 start_date = '2010-01-01'
 trade_date = '2023-07-01'
+end_date = '2023-12-31'
 
 # freqs = {'1m': 1, '3m':3, '5m':5, '15m':15, '30m':30, '1h':60, '2h':120, '4h':240, '6h':360, '8h':480, '12h':720, '1d':1440}
 # Because we want as much data as possible, it makes sense to use only 1m
